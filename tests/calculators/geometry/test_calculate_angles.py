1a29e26fe10e0b21702ba2edd798ff2cf42f92f3
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-12-06 20:02:11 +0000

    Remove hardcoded test outputs.

--- tests/calculators/geometry/test_calculate_angles.py
+++ tests/calculators/geometry/test_calculate_angles.py
@@ -1,3 +1,6 @@
+import json
+import pathlib
+
 import numpy as np
 
 import stko
@@ -9,12 +12,18 @@ def test_calculate_angles(case_data: CaseData) -> None:
     analyser = stko.molecule_analysis.GeometryAnalyser()
 
     result = analyser.calculate_angles(case_data.molecule)
+
+    parent = pathlib.Path(__file__).resolve().parent
+    saved_dir = parent / "geom_jsons"
+    saved_json = saved_dir / f"{case_data.name}_angles.json"
+    if not saved_json.exists():
+        with saved_json.open("w") as f:
+            json.dump({"_".join(i): j for i, j in result.items()}, f, indent=4)
+        raise AssertionError
+
+    with saved_json.open("r") as f:
+        known = {tuple(i.split("_")): j for i, j in json.load(f).items()}
+
     for triple in result:
-        if triple == ("C", "C", "C"):
-            continue
-        if "H" in triple:
-            continue
-        assert len(result[triple]) == len(case_data.angles[triple])
-        assert np.allclose(
-            result[triple], case_data.angles[triple], rtol=0, atol=1e-3
-        )
+        assert len(result[triple]) == len(known[triple])
+        assert np.allclose(result[triple], known[triple], rtol=0, atol=1e-3)
--- tests/calculators/geometry/test_calculate_bonds.py
+++ tests/calculators/geometry/test_calculate_bonds.py
@@ -1,3 +1,6 @@
+import json
+import pathlib
+
 import numpy as np
 
 import stko
@@ -9,12 +12,18 @@ def test_calculate_bonds(case_data: CaseData) -> None:
     analyser = stko.molecule_analysis.GeometryAnalyser()
 
     result = analyser.calculate_bonds(case_data.molecule)
+
+    parent = pathlib.Path(__file__).resolve().parent
+    saved_dir = parent / "geom_jsons"
+    saved_json = saved_dir / f"{case_data.name}_bonds.json"
+    if not saved_json.exists():
+        with saved_json.open("w") as f:
+            json.dump({"_".join(i): j for i, j in result.items()}, f, indent=4)
+        raise AssertionError
+
+    with saved_json.open("r") as f:
+        known = {tuple(i.split("_")): j for i, j in json.load(f).items()}
+
     for pair in result:
-        if pair == ("C", "C"):
-            continue
-        if "H" in pair:
-            continue
-        assert len(result[pair]) == len(case_data.bonds[pair])
-        assert np.allclose(
-            result[pair], case_data.bonds[pair], rtol=0, atol=1e-3
-        )
+        assert len(result[pair]) == len(known[pair])
+        assert np.allclose(result[pair], known[pair], rtol=0, atol=1e-3)
